499a2c6b44e0d6c3b8534474b912918b51d57a20
Tariro Mukute <tariromukute@gmail.com>
2023-03-17 01:09:22 +0200

    Update to collect results for Open5gs

---
+++ ansible/exec.sh
@@ -0,0 +1,24 @@
+
+# This script is used to execute the commands in to run ansible ad hoc commands
+# and playbooks.
+
+# List of syscalls to inspect
+N_UES=300
+SYSCALLS="futex epoll_wait recvmsg clock_nanosleep poll select ppoll read openat sendto sched_yield recvfrom"
+# Loop through the syscalls and run the ansible ad hoc commands with syscall as parameter
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
+    -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py -d 20 -L -m -j", tool: syscount, ues: '$N_UES' }'
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
+    -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py -d 20 -L -P -m -j", tool: sysprocess, ues: '$N_UES' }'
+
+for SYSCALL in $SYSCALLS; do
+    ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
+    -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py --syscall '$SYSCALL' -d 20 -L -P -m -j", tool: sysprocess_'$SYSCALL', ues: '$N_UES' }'
+done
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_enter_epoll_wait():u16:args->timeout' -i 20 -d 20\", tool: sysprocess_enter_epoll_wait_timeout, ues: "$N_UES" }"
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_epoll_wait():u16:args->ret' -i 20 -d 20\", tool: sysprocess_exit_epoll_wait, ues: "$N_UES" }"
