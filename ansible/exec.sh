aa12292b9a8544d7fdea698d19ba0fabd1315127
Tariro Mukute <tariromukute@gmail.com>
2023-03-27 20:03:42 +0200

    Updated to collect free5gc perfromance results

--- ansible/exec.sh
+++ ansible/exec.sh
@@ -3,25 +3,25 @@
 # and playbooks.
 
 # List of syscalls to inspect
-NX="0 10 20 40 50 60 80 100"
+NX="10 30 70"
 for N_UES in $NX; do
-# SYSCALLS="futex epoll_wait recvmsg clock_nanosleep poll select ppoll read openat sendto sched_yield recvfrom fdatasync write nanosleep"
-SYSCALLS="fdatasync write nanosleep"
+SYSCALLS="futex epoll_wait recvmsg clock_nanosleep poll select ppoll read openat sendto sched_yield recvfrom fdatasync write nanosleep"
+# SYSCALLS="fdatasync write nanosleep"
 # Loop through the syscalls and run the ansible ad hoc commands with syscall as parameter
-# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
-#     -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py -d 20 -L -m -j", tool: syscount, ues: '$N_UES' }'
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py -d 20 -L -m -j", tool: syscount, ues: '$N_UES' }'
 
-# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
-#     -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py -d 20 -L -P -m -j", tool: sysprocess, ues: '$N_UES' }'
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py -d 20 -L -P -m -j", tool: sysprocess, ues: '$N_UES' }'
 
 for SYSCALL in $SYSCALLS; do
     ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
     -e '{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: "syscount.py --syscall '$SYSCALL' -d 20 -L -P -m -j", tool: sysprocess_'$SYSCALL', ues: '$N_UES' }'
 done
 
-# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
-#     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_enter_epoll_wait():u16:args->timeout' -i 20 -d 20\", tool: sysprocess_enter_epoll_wait_timeout, ues: "$N_UES" }"
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_enter_epoll_wait():u16:args->timeout' -i 20 -d 20\", tool: sysprocess_enter_epoll_wait_timeout, ues: "$N_UES" }"
 
-# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
-#     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_epoll_wait():u16:args->ret' -i 20 -d 20\", tool: sysprocess_exit_epoll_wait, ues: "$N_UES" }"
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_epoll_wait():u16:args->ret' -i 20 -d 20\", tool: sysprocess_exit_epoll_wait, ues: "$N_UES" }"
 done
