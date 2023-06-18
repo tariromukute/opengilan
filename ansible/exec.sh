435afaff4efdb7ad44b20e73fda92cdcd59ba3f0
Tariro Mukute <tariromukute@gmail.com>
2023-06-18 13:49:49 +0200

    Seperate free5gc bcc and bpftrace scirpts for collecting results

--- ansible/exec.sh
+++ ansible/exec.sh
@@ -24,4 +24,23 @@ ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.ym
 
 ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_epoll_wait():u16:args->ret' -i 20 -d 20\", tool: sysprocess_exit_epoll_wait, ues: "$N_UES" }"
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_nanosleep():u16:args->ret' -i 20 -d 20\", tool: sys_exit_nanosleep, ues: "$N_UES" }"
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_clock_nanosleep():u16:args->ret' -i 20 -d 20\", tool: sys_exit_clock_nanosleep, ues: "$N_UES" }"
+
+# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+#     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"trace.py -U -a -A -M 1000 'r::__x64_sys_futex "%d", retval' -i 20 -d 20\", tool: __x64_sys_futex, ues: "$N_UES" }"
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"klockstat.py -d 20\", tool: sys_exit_clock_nanosleep, ues: "$N_UES" }"
+
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -c -C 't:syscalls:sys_enter_socket():int,int,int:$PID,args->protocol,args->family&00004000' -i 20 -d 20\", tool: sys_enter_socket, ues: "$N_UES" }"
+
+PID='$PID'
+ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -c -C 't:syscalls:sys_enter_accept4():int,int,int:$PID,args->fd,args->flags&00004000' -i 20 -d 20\", tool: sys_enter_accept4, ues: "$N_UES" }"
 done
