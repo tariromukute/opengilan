a7d43b30890e21c4e49691a29252692dc0fd3c28
Tariro Mukute <tariromukute@gmail.com>
2023-06-20 01:27:37 +0200

    Fix naming

--- ansible/exec.sh
+++ ansible/exec.sh
@@ -35,17 +35,18 @@ ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.ym
 ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_clock_nanosleep():u16:args->ret' -i 20 -d 20\", tool: sys_exit_clock_nanosleep, ues: "$N_UES" }"
 
-# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/free5gc.yml \
+# ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
 #     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"trace.py -U -a -A -M 1000 'r::__x64_sys_futex "%d", retval' -i 20 -d 20\", tool: __x64_sys_futex, ues: "$N_UES" }"
 
 ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
-    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"klockstat.py -d 20\", tool: sys_exit_clock_nanosleep, ues: "$N_UES" }"
+    -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"klockstat.py -d 20\", tool: klockstat, ues: "$N_UES" }"
 
 ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -c -C 't:syscalls:sys_enter_socket():char*,int,int:$COMM,args->protocol,args->family&00004000' -i 20 -d 20\", tool: sys_enter_socket, ues: "$N_UES" }"
 
 ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/open5gs.yml \
     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -c -C 't:syscalls:sys_enter_accept4():char*,int,int:$COMM,args->fd,args->flags&00004000' -i 20 -d 20\", tool: sys_enter_accept4, ues: "$N_UES" }"
+
 # ansible all -i inventory.ini -u ubuntu -m include_tasks -a file=plays/oai-1.yml \
 #     -e "{ user: ubuntu,  duration: 20, aduration: 35, interval: 0, tool_cmd: \"argdist.py -C 't:syscalls:sys_exit_epoll_wait():u16:args->ret' -i 20 -d 20\", tool: sysprocess_exit_epoll_wait, ues: "$N_UES" }"
 done
